c8b161f64dd2cb9783e19f2bc24607dab371a547
Lochan Patil <lochangpatil@gmail.com>
2026-08-07 19:25:50 +0530

    frontend base ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () =>{
+  return{
+    <div>
+      <h1>JeevaSakha</h1>
+    </div>
+  }
+}
+
+export default App;
